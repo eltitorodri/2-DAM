0d45c0ef2c8efc0306b7b570d86eac3f1d0b8a5e
eltitorodri <rbernalgomez@safareyes.es>
2025-10-14 10:29:33 +0200

    Salvaresguardo login

--- Diseño Interfaces/swapy/src/app/sign-up/sign-up.component.ts
+++ Diseño Interfaces/swapy/src/app/sign-up/sign-up.component.ts
@@ -1,22 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import {IonicModule} from "@ionic/angular";
-import {LogoConFondoComponent} from "../logo-con-fondo/logo-con-fondo.component";
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { LogoConFondoComponent } from "../logo-con-fondo/logo-con-fondo.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
   standalone: true,
-    imports: [
+  imports: [
     IonicModule,
-    LogoConFondoComponent,
+    FormsModule,
+    LogoConFondoComponent
   ]
 })
-export class SignUpComponent  implements OnInit {
+export class SignUpComponent {
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
-  ngOnInit() {}
+  get passwordsMatch(): boolean {
+    return this.password.length > 0 && this.password === this.confirmPassword;
+  }
 
+  get hasMinLength(): boolean {
+    return this.password.length >= 8;
+  }
+
+  get isValidEmail(): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(this.email);
+  }
+
+  get isFormValid(): boolean {
+    return this.passwordsMatch && this.hasMinLength && this.isValidEmail;
+  }
+
+  onSubmit() {
+    if (this.isFormValid) {
+      console.log('Formulario válido', {
+        email: this.email,
+        password: this.password
+      });
+    }
+  }
 }
