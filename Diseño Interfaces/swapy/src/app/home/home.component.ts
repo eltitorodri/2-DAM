eddb1d86fc5c9073217945844154e943e01326a8
eltitorodri <rbernalgomez@safareyes.es>
2025-10-14 09:26:16 +0200

    Salvaresguardo login

--- Diseño Interfaces/swapy/src/app/home/home.component.ts
+++ Diseño Interfaces/swapy/src/app/home/home.component.ts
@@ -1,16 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {IonicModule} from "@ionic/angular";
-import {LogoConFondoComponent} from "../logo-con-fondo/logo-con-fondo.component";
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-login',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [
-    IonicModule,
-    LogoConFondoComponent,
-  ]
 })
 
 export class HomeComponent  implements OnInit {
---
+++ Diseño Interfaces/swapy/src/app/sign-up/sign-up.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { SignUpComponent } from './sign-up.component';
+
+describe('SignUpComponent', () => {
+  let component: SignUpComponent;
+  let fixture: ComponentFixture<SignUpComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [SignUpComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SignUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
