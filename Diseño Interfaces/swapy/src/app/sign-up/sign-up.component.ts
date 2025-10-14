d1ccb256cb5d8dfaa11735cf45e09f15791de9bb
eltitorodri <rbernalgomez@safareyes.es>
2025-10-14 09:59:42 +0200

    Salvaresguardo login

--- Diseño Interfaces/swapy/src/app/sign-up/sign-up.component.ts
+++ Diseño Interfaces/swapy/src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
+import {LogoConFondoComponent} from "../logo-con-fondo/logo-con-fondo.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
   standalone: true,
+    imports: [
+    IonicModule,
+    LogoConFondoComponent,
+  ]
 })
 export class SignUpComponent  implements OnInit {
 
