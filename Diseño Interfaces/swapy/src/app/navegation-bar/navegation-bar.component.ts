0f07d36d94650c1713fd75edb90a9cf3ebc0689c
eltitorodri <rbernalgomez@safareyes.es>
2025-10-20 11:16:45 +0100

    Subida de diseño de BBDD

--- Diseño Interfaces/swapy/src/app/navegation-bar/navegation-bar.component.ts
+++ Diseño Interfaces/swapy/src/app/navegation-bar/navegation-bar.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { IonContent, IonFooter, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonFooter, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-navegation-bar',
   templateUrl: './navegation-bar.component.html',
   styleUrls: ['./navegation-bar.component.scss'],
   standalone: true,
-  imports: [IonContent, IonFooter, IonTitle, IonToolbar],
+  imports: [IonFooter, IonTitle, IonToolbar],
 })
 export class NavegationBarComponent  implements OnInit {
 
