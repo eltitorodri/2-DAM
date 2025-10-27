c491e17467ec9c4df277d4d2744141b82b543d47
eltitorodri <rbernalgomez@safareyes.es>
2025-10-27 11:57:21 +0000

    Actualizacion del lunes

--- Diseño Interfaces/swapy/src/app/app.routes.ts
+++ Diseño Interfaces/swapy/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { NavegationBarComponent } from './navegation-bar/navegation-bar.component';
+import { SplashComponent } from './splash/splash.component';
 
 export const routes: Routes = [
 
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '', redirectTo: '/splash', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
   { path: 'sign-up', component: SignUpComponent },
   { path: 'navegation-bar', component: NavegationBarComponent },
+  { path: 'splash', component: SplashComponent },
 ];
