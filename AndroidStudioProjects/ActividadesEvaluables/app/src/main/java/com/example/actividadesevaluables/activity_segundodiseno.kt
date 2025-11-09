b68da751c1535ed4c432e9b5948181d34c70f6a7
eltitorodri <rbernalgomez@safareyes.es>
2025-11-09 14:39:21 +0100

    Android Studio Entero

---
+++ AndroidStudioProjects/ActividadesEvaluables/app/src/main/java/com/example/actividadesevaluables/activity_segundodiseno.kt
@@ -0,0 +1,18 @@
+package com.example.actividadesevaluables
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.actividadesevaluables.R.id.main
+
+class activity_segundodiseno : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_segundodiseno)
+    }
+}
