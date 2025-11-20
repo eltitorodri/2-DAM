5dc8bc350a77b7426cf7ff734b23c4616f431960
eltitorodri <rbernalgomez@safareyes.es>
2025-11-20 10:44:00 +0000

    git push clase

--- Diseño Interfaces/SwapyBackend/swapy/src/test/java/com/example/swapy/SwapyApplicationTests.java
+++ Diseño Interfaces/SwapyBackend/swapy/src/test/java/com/example/swapy/SwapyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.swapy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SwapyApplicationTests {
 
 	@Test
 	void contextLoads() {
