ee74af2c73b73e33c2e1e6c1216754a128822bcc
eltitorodri <rbernalgomez@safareyes.es>
2025-11-24 13:23:28 +0000

    Subida de imagenes de la clase de Luis

---
+++ Diseño Interfaces/SwapyBackend/swapy/src/main/java/com/example/swapy/dto/UsuarioDTO.java
@@ -0,0 +1,19 @@
+package com.example.swapy.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+
+    private Integer id;
+    private String nombreCompleto;
+    private String passwordHash;
+    private String nickname;
+    private String email;
+
+}
